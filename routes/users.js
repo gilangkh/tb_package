6ee54f854b97ce2c329bca339f76d5c7c1186499
Gilang2812 <100253104+Gilang2812@users.noreply.github.com>
2023-10-04 19:12:57 +0700

    form

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+const path = require('path')
+const link = path.join(__dirname,'../public/pages/')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/index', function(req, res, next) {
+  res.sendFile(link+"login.html")
+});
+
 
 
 
